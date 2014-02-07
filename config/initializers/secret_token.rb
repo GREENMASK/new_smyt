b861c4f071698ad819f8eb919cf982171e062494
GreenMask <m88piotr@gmail.com>
2014-02-07 15:35:08 +0100

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smyt::Application.config.secret_key_base = '1f47f6d4c167e79474c5e2c6478312403deec2596054df05314a36a9ca1c3c79d627a39dc8fb1309d05efe6da95cd405ffc71df4342fdef155c2925429d8112c'
